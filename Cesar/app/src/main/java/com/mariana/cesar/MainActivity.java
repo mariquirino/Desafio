ebae3164a61c775cc1451ebef06a84bc961b5824
Mariana <quirino193@gmail.com>
2019-05-24 18:25:14 -0300

    refactoring and more examples

--- Cesar/app/src/main/java/com/mariana/cesar/MainActivity.java
+++ Cesar/app/src/main/java/com/mariana/cesar/MainActivity.java
@@ -16,10 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private RecyclerView recyclerView;
-    private RecyclerView.Adapter mAdapter;
     private MyAdapter adapter;
-    private RecyclerView.LayoutManager layoutManager;
 
     final List<String> myDataset = new ArrayList<>();
 
@@ -33,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         myDataset.add("Teste2");
         myDataset.add("Outro");
         myDataset.add("You");
+        myDataset.add("mariana");
 
 
         setUpRecyclerView();
@@ -42,27 +40,10 @@ public class MainActivity extends AppCompatActivity {
         Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-//                List<String> words = new ArrayList<>();
-
-//                for (String word : myDataset) {
-//                    Verify verify = new Verify();
-//                    //Verificar pois so pega a palavra correta
-//                    if (verify.verifyPermuted(word, editText.getText().toString()) || verify.verifyTypo(word, editText.getText().toString())) {
-////                        if (verify.verifyTypo(word, editText.getText().toString())) {
-//                        words.add(word);
-////                        }
-//                    }
-//                }
-
-//                mAdapter = new Adapter(words);
-//                adapter = new Adapter(myDataset);
-
-                EditText editText = (EditText) findViewById(R.id.search);
+                EditText editText = findViewById(R.id.search);
                 adapter.getFilter().filter(editText.getText().toString());
             }
         });
-//        mAdapter = new Adapter(myDataset);
-//        recyclerView.setAdapter(mAdapter);
     }
 
     private void setUpRecyclerView() {
